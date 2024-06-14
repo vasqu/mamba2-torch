5496895c64e5f1b42540a0a7f302648a66c891b9
Vasqu <antonprogamer@gmail.com>
2024-06-14 20:42:05 +0200

    check if last states are the same too

--- tests/TestSSDMinimal.py
+++ tests/TestSSDMinimal.py
@@ -25,12 +25,14 @@ D = torch.randn(nheads, dtype=dtype, device=device)
 initial_state = torch.randn(batch, nheads, headdim, dstate, dtype=dtype, device=device)
 
 # Comparing fused version and minimal version
-y = mamba_chunk_scan_combined(x, dt, A, B, C, chunk_size, D=D, initial_states=initial_state)
+y, l = mamba_chunk_scan_combined(x, dt, A, B, C, chunk_size, D=D, initial_states=initial_state, return_final_states=True)
 y_2 = mamba_chunk_scan_combined(x, dt, A, B, C, chunk_size, D=None, initial_states=initial_state)
-y_min, test = ssd_minimal_discrete(x, dt, A, B, C, chunk_size, D=D, initial_states=rearrange(initial_state, "b n h d -> b 1 n h d"))
+y_min, l_min = ssd_minimal_discrete(x, dt, A, B, C, chunk_size, D=D, initial_states=rearrange(initial_state, "b n h d -> b 1 n h d"))
 y_min_2, _ = ssd_minimal_discrete(x, dt, A, B, C, chunk_size, D=None, initial_states=rearrange(initial_state, "b n h d -> b 1 n h d"))
 
 
+print(torch.allclose(l, l_min, atol=0.1, rtol=0.1))
+print(torch.allclose(l, l_min, atol=0.01, rtol=0.01))
 print(torch.allclose(y_2, y_min_2, atol=0.1, rtol=0.1))
 print(torch.allclose(y_2, y_min_2, atol=0.01, rtol=0.01))
 print(torch.allclose(y, y_min, atol=0.1, rtol=0.1))
