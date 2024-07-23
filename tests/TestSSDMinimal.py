8df4df68b4991f4d5f5721cb7f87bcc06c5e6ad5
Vasqu <antonprogamer@gmail.com>
2024-07-23 16:15:31 +0200

    check for group size > 1, remove lower bounds in test

--- tests/TestSSDMinimal.py
+++ tests/TestSSDMinimal.py
@@ -11,7 +11,7 @@ torch.manual_seed(42)
 # Denoted (B, T, Q, D, P) in the paper
 batch, seqlen, chunk_size, dim, headdim = 1, 100, 64, 2048, 64
 nheads = dim // headdim  # (H) in the paper
-ngroups = 1 # (G) in the paper
+ngroups = 8 # (G) in the paper
 dstate = 64  # (N) in the paper
 dtype = torch.float32
 device = "cuda"
@@ -19,8 +19,8 @@ device = "cuda"
 x = torch.randn(batch, seqlen, nheads, headdim, dtype=dtype, device=device)
 dt = F.softplus(torch.randn(batch, seqlen, nheads, dtype=torch.float32, device=device) - 4).requires_grad_()
 A = (-torch.exp(torch.rand(nheads, dtype=torch.float32, device=device))).requires_grad_()
-B = torch.randn(batch, seqlen, ngroups, dstate, dtype=dtype, device=device)
-C = torch.randn(batch, seqlen, ngroups, dstate, dtype=dtype, device=device)
+B = torch.randn(batch, seqlen, ngroups, dstate, dtype=dtype, device=device).repeat(1, 1, nheads // ngroups, 1)
+C = torch.randn(batch, seqlen, ngroups, dstate, dtype=dtype, device=device).repeat(1, 1, nheads // ngroups, 1)
 D = torch.randn(nheads, dtype=dtype, device=device)
 initial_state = torch.randn(batch, nheads, headdim, dstate, dtype=dtype, device=device)
 
@@ -31,9 +31,6 @@ y_min, l_min = ssd_minimal_discrete(x, dt, A, B, C, chunk_size, D=D, initial_sta
 y_min_2, _ = ssd_minimal_discrete(x, dt, A, B, C, chunk_size, D=None, initial_states=rearrange(initial_state, "b n h d -> b 1 n h d"))
 
 
-print(torch.allclose(l, l_min, atol=0.1, rtol=0.1))
 print(torch.allclose(l, l_min, atol=0.01, rtol=0.01))
-print(torch.allclose(y_2, y_min_2, atol=0.1, rtol=0.1))
-print(torch.allclose(y_2, y_min_2, atol=0.01, rtol=0.01))
-print(torch.allclose(y, y_min, atol=0.1, rtol=0.1))
 print(torch.allclose(y, y_min, atol=0.01, rtol=0.01))
+print(torch.allclose(y_2, y_min_2, atol=0.01, rtol=0.01))
